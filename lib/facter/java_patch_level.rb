aba3e2c4dc291af4758a505b271e9d250ea931c8
Peter Souter <p.souter@kainos.com>
2014-12-04 21:16:50 +0000

    Add patch level patch

---
+++ lib/facter/java_patch_level.rb
@@ -0,0 +1,23 @@
+# Fact: java_patch_level
+#
+# Purpose: store java patch level
+#
+# Resolution:
+#   tests for presence of java;
+#   returns output of "java -version" and splits on \n + '"'
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
+Facter.add(:java_patch_level) do
+  setcode do
+    t_java = Facter::Util::Resolution.exec("java -version 2>&1")
+    if t_java.nil?
+      java_version = "NOT_INSTALLED"
+    else
+      j_version = t_java.to_s.lines.first.strip.split(/version/)[1].gsub(/"/, "").strip.split('_')
+    end
+  end
+end
